58c63ca1a1701b1487b975e9e3183508effd3833
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-07 21:34:56 -0300

    :chess_pawn: aula3

---
+++ Introducao/aula3.py
@@ -0,0 +1,20 @@
+'''
+Python é uma linguagem de programação de tipagem dinâmica e forte.
+Tipagem dinâmica o python já sabe o tipo de informação que você está passando para ele.
+Exemplo: print(1234) o python identifica que será printado um número.
+
+Strings
+Python aceita strings dentro de aspas simples e duplas.
+'''
+
+#Escape
+#Identifica o caracter de escape e faz com que o interpretador "ignore-o".
+print("Henrique \"Azevedo\"")
+
+# r
+# Faz com que o caracter de escape seja exibido.
+print(r"Henrique \"Azevedo\"")
+
+# Jeito esperto
+print('Henrique "Azevedo"')
+print('Explícito', 'é', 'melhor " do que implícito')
