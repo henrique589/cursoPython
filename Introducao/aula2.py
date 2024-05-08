3172b881c15fc670a265a52f87171b0ca7975ba4
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-07 21:20:01 -0300

    :chess_pawn: aula2

---
+++ Introducao/aula2.py
@@ -0,0 +1,6 @@
+# O argumento nomeado 'sep' nos indica qual separador será utilizado.
+# No exemplo abaixo foi utilizado -, mas poderia ser deixado vazio.
+# E o argumento nomeado 'end' nos auxilia a modificar o final de linha.
+# Python é case sensitive
+print(12, 34, sep='-', end='\n##')
+print(56, 78, sep='-')
