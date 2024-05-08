b45ffd5a119ea7aa81ee47596601b78ace9ac5c7
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-07 21:09:03 -0300

    :chess_pawn: aula1

---
+++ Introducao/aula1.py
@@ -0,0 +1,15 @@
+# Comentário
+# O interpretador do python não lê essas linhas.
+print("Hello World!")
+
+"""
+Docstrings não é um comentário.
+Mas funciona como um comentário.
+O interpretador do python lê essas linhas.
+"""
+
+print(123)
+
+'''
+Docstrings com aspas simples.
+'''
