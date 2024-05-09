72cc7f3fb93dd4ebaf2eaa7ef861d58c1959e10e
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-08 21:15:41 -0300

    :chess_pawn: aula5

---
+++ Introducao/aula5.py
@@ -0,0 +1,8 @@
+"""
+Tipo de dado boll (boolean).
+(True) ou (False).
+== é o operador lógico que irá comparar os valores.
+"""
+print(type(10 == 10))
+print(10 == 10)
+print("Henrique" == "Henrique")
