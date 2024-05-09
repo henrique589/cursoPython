29fa6527bf2b86d28819d8332a3bb5f99acf8247
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-08 22:08:41 -0300

    :chess_pawn: aula10

---
+++ Introducao/aula10.py
@@ -0,0 +1,10 @@
+# O sinal de + pode ser usado para fazer concatenação com strings.
+
+concatenacao = 'Henrique' + ' ' + 'Azevedo'
+print(concatenacao)
+
+# O sinal de multiplicação pode fazer a repetição de algum elemento.
+a_dez_vezes = 'A ' * 10
+tres_vezes_henrique = 'Henrique' * 3
+print(a_dez_vezes)
+print(tres_vezes_henrique)
