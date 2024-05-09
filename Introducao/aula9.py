dcef0d163fe62adbe7488916c3a668ba61ac0ace
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-08 22:05:03 -0300

    :chess_pawn: aula9

---
+++ Introducao/aula9.py
@@ -0,0 +1,8 @@
+# Operadores aritméticos
+#Adição +
+#Subtração -
+#Multiplicação *
+#Divisão / o retorno sempre será um valor de ponto flutuante.
+#Divisão inteira // faz o truncamento
+#Exponenciação **
+#Módulo % -> resto da divisão
