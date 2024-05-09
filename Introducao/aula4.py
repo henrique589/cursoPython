da45a83ed651c721830de138d0efa9d6e219cd72
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-08 21:07:28 -0300

    :chess_pawn: aula4

---
+++ Introducao/aula4.py
@@ -0,0 +1,11 @@
+#Tipos int e float
+"""
+int representa os números inteiros.
+float representa os números com ponto flutuante.
+
+A classe type mostra o tipo que o Python inferiu ao valor.
+Tudo em Python é um objeto.
+"""
+print(type(1))
+print(type('Henrique'))
+print(type(1.1), type(-1.1), type(0.0))
