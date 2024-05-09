b75bc4c7c1ed9d8569607487e4f1138a74d85afd
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-08 21:50:11 -0300

    :chess_pawn: aula7

---
+++ Introducao/aula7.py
@@ -0,0 +1,11 @@
+# Variáveis são usadas para salvar dados na memória do computador.
+
+#nome_completo = 'Henrique Azevedo Andrade Silva'
+#soma_dois_mais_dois = 2 + 2
+# print(nome_completo, soma_dois_mais_dois, sep=', ')
+
+nome = 'Henrique'
+idade = 21
+maior_de_idade = idade >= 18
+print('Nome:', nome, ', Idade:', idade)
+print('É maior?', maior_de_idade)
