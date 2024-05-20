fdc696cdff88fe14f8de316792ea7557c9778024
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-20 10:46:17 -0300

    :chess_pawn: aula25

---
+++ Introducao/aula25.py
@@ -0,0 +1,12 @@
+"""
+s - string
+d e i - int
+f - float
+x e X - hexadecimal
+"""
+
+nome = 'Henrique'
+preco = 1000.9589
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %04x' % (15, 15))
