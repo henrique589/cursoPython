1588123746e05651003d6d6452fd952ba60f938a
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-21 11:06:07 -0300

    :chess_pawn: aula32 solução 1

---
+++ Introducao/aula32.py
@@ -0,0 +1,27 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+numero = input('Digite um número inteiro: ')
+try:    
+    numero_int = float(numero)
+    if(numero_int % 2 == 0):
+        print(f'O número {numero_int} é par!')
+    else:
+        print(f'O número {numero_int} é ímpar!')
+except:
+    print('Não digitou um número inteiro')
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
