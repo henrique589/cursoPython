3e9ca17f35d5e87b67809f8728864ad788c13e64
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-21 10:50:02 -0300

    :chess_pawn: aula31

--- Introducao/aula31.py
+++ Introducao/aula31.py
@@ -6,6 +6,21 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
 
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não entrou no if')
+
+if passou_no_if is not None:
+    print('Entrou no if')
+
 v1 = 'a'
 v2 = 'a'
 print(id(v1))
