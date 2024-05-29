0f252dcbb9329bb9d4eb4175034abc827e9a0b27
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-05-29 09:31:08 -0300

    :chess_pawn: aula48

--- Introducao/aula48.py
+++ Introducao/aula48.py
@@ -14,14 +14,33 @@ lista[2] = 'João'
 print(lista)
 print(lista[2], type(lista[2]))"""
 
-lista = [10, 20, 30, 40]
+
 """lista[2] = 300
 del lista[2]
 print(lista)
-print(lista[2])"""
+print(lista[2])
 lista.append(50)
 lista.pop()
 lista.append(60)
 lista.append(70)
 ultimo_valor = lista.pop(3)
-print(lista, 'Removido,', ultimo_valor)
+print(lista, 'Removido,', ultimo_valor)"""
+
+"""
+append - adiciona um item no final
+insert - adiciona um item no índice escolhido
+pop - remove do final ou do índice escolhido
+del - apaga um índice
+clear - limpa a lista
+extend - estende a lista
++ - concatena a lista
+
+"""
+
+lista = [10, 20, 30, 40]
+lista.append('Henrique')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+lista.insert(0, 5)
+print(lista)
