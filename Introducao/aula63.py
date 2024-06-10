1c48a13ac2774ada77cdb8493df887634f300131
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-06-10 10:09:22 -0300

    :chess_pawn: aula63

---
+++ Introducao/aula63.py
@@ -0,0 +1,35 @@
+"""
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF mais o primeiro dígito
+multiplicando cada um dos valores por uma contageem regressiva
+começando de 10
+"""
+import random
+
+cpf_9_digitos = ''
+for i in range(0, 9):
+    cpf_9_digitos += str(random.randint(0, 9))
+
+contador_1 = 10
+resultado_1 = 0
+
+for digito in cpf_9_digitos:
+    resultado_1 += int(digito) * contador_1
+    contador_1 -= 1
+
+digito_1 = (resultado_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+cpf_10_digitos = cpf_9_digitos + str(digito_1)
+contador_2 = 11
+resultado_2 = 0
+
+for digito in cpf_10_digitos:
+    resultado_2 += int(digito) * contador_2
+    contador_2 -= 1
+
+digito_2 = (resultado_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+novo_cpf = f'{cpf_9_digitos}{digito_1}{digito_2}'
+print(novo_cpf)
