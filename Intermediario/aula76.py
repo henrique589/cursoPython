0691e33e89b54a9cf7cee7a2c506b0ad37ed923e
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-06-13 10:49:46 -0300

    :pleading_face: aula76 (Dicionários)

---
+++ Intermediario/aula76.py
@@ -0,0 +1,27 @@
+# Dicionário em Python (tipo dict)
+# Dicionários são estruturas de dados do tipo
+# par de "chave" e "valor".
+# Chaves podem ser consideradas como "índice"
+# que vimos na lista e podem ser de tipos imutáveis
+# como: str, int, floaat, bool, tuple, etc.
+# O valor pode ser de qualquer tipo, incluindo outro dicionário.
+# Usamos as chaves - {} - ou a classe dict para criar 
+# dicionários.
+# Imutáveis: str, int, float, boll, tuple
+# Mutável: dict, list
+"""pessoa = {
+    'nome': 'Henrique',
+    'sobrenome': 'Azevedo',
+    'idade': 21,
+    'altura': 1.85,
+    'enderecos': [
+        {'rua': 'tal qual', 'numero': 123},
+        {'rua': 'outra rua', 'numero': 321},
+    ]
+}
+
+print(pessoa, type(pessoa))
+print(pessoa['nome'])
+
+for chave in pessoa:
+    print(chave, pessoa[chave])"""
