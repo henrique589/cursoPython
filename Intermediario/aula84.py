9c140c6cfa29451d2acdd8547db8c472c118f8d1
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-06-26 10:07:20 -0300

    :pleading_face: aula84 (Filtro de dados em list comprehension)

--- Intermediario/aula84.py
+++ Intermediario/aula84.py
@@ -3,6 +3,11 @@
 # a partir de iteráveis.
 
 # print(list(range(10)))
+import pprint
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
+
 lista = []
 for numero in range(10):
     lista.append(numero)
@@ -27,4 +32,15 @@ novos_produtos = [
     if produto['preco'] > 20 else {**produto}
     for produto in produtos
 ]
-print(*novos_produtos, sep='\n')
+# print(*novos_produtos, sep='\n')
+# p(novos_produtos)
+# lista = [n for n in range(10) if n < 5]
+
+novos_produtos = [
+    # {'nome': produto['nome'], 'preco': produto['preco']}
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+    if produto['preco'] > 10
+]
+p(novos_produtos)
