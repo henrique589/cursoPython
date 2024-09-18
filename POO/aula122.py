e4eccba6f5df9dbf8f7819ccf7ad2afc0b78e7ff
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-09-18 20:24:19 -0300

    :sparkles: Sobre self

---
+++ POO/aula122.py
@@ -0,0 +1,16 @@
+# Entendendo self em classes Python
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro('Celta')
+print(celta.nome)
+celta.acelerar()
