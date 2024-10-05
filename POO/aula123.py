5e7f388c415e9c6797ece2b66e6d1bd87cac9ae3
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-10-05 18:26:42 -0300

    :sparkles: aula 123 (Escopo de classe e métodos de classe)

---
+++ POO/aula123.py
@@ -0,0 +1,18 @@
+# Escopo da classe e de método da classe
+
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+
+        # variavel = 'valor'
+        # print(variavel)
+
+    def comendo(self, alimento):
+        return f'{self.nome} está comendo {alimento}'
+    
+    def executar(self, *args, **kwargs):
+        return self.comendo(*args, **kwargs)
+
+leao = Animal(nome='Leão')
+print(leao.nome)
+print(leao.executar('xuxu'))
