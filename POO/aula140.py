6d4c5b217a28ca42444e32cb7dd83bef5cb19eff
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-10-26 13:56:49 -0300

    :sparkles: aula140 (Herança múltipla - teoria)

---
+++ POO/aula140.py
@@ -0,0 +1,12 @@
+# Herança múltipla - Python Orientado a Objetos
+# Quer dizer que no Python, uma classe pode estender
+# várias outras classes.
+# Herança simples:
+# Animal -> Mamífero -> Humano -> Pessoa -> Cliente
+# Herança múltipla e mixins
+# Log -> Filelog
+# Animal -> Mamífero -> Humano -> Pessoa -> Cliente
+# Cliente(Pessoa, Filelog)
+
+# Classe.mro()
+# __mro__
