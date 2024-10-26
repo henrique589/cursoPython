045342ae3af99b5b3dd2bd71a1e239a1e06bf88e
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-10-26 17:45:07 -0300

    :sparkles: aula140 (Herança múltipla)

--- POO/aula140.py
+++ POO/aula140.py
@@ -10,3 +10,31 @@
 
 # Classe.mro()
 # __mro__
+
+class A:
+    pass
+
+    def quem_sou(self):
+        print('A')
+
+class B(A):
+    pass
+
+    # def quem_sou(self):
+    #     print('B')
+
+class C(A):
+    pass
+
+    def quem_sou(self):
+        print('C')
+
+class D(B, C):
+    pass
+
+    # def quem_sou(self):
+    #     print('D')
+
+d = D()
+d.quem_sou()
+print(D.__mro__)
