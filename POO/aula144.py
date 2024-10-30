173cc260b54c4eeb7093af180d935baa7a5a4c56
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-10-30 11:44:28 -0300

    :sparkles: aula144 (Polimorfismo)

---
+++ POO/aula144.py
@@ -0,0 +1,43 @@
+# Polimorfismo em Python Orientado a Objetos
+# Polimorfismo é o princípio que permite que classes derivadas
+# de uma mesma superclasse tenham métodos iguais (com a mesma assinatura)
+# mas comportamentos diferentes.
+# Assinatura do método = Mesmo nome e quantidade de parâmetros
+# (retorno não faz parte da assinatura)
+# Opinião + princípios que contam: 
+# Assinatura do método: nome, parâmetros e retornos iguais
+# SO"L"ID
+# Princípio da substituição da liskov
+# Objetos de uma superclasse devem ser subtituíveis
+# por objetos de uma subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload)
+# Sobreposição de métodos (override)
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool:
+        ...
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('Email: enviando - :', self.mensagem)
+        return True
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando - :', self.mensagem)
+        return False
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada')
+    else:
+        print('Notificação não enviada')
+
+notificar(NotificacaoEmail('testando email'))
